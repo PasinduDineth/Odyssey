ede932e6b9b00232b597778052c248987579eb90
Pasindu dineth <pasindudinethpeiris@gmail.com>
2023-12-14 14:16:06 -0600

    feat: sidebar improvements

--- odyssey-ui/src/app/layout.js
+++ odyssey-ui/src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={`flex flex-col min-h-screen overflow-hidden`}>
         <Navbar toggleSidebar={toggleSidebar} isOpen={isOpen}/>
         <div className='flex flex-1 overflow-hidden'>
-          <div className={`flex-shrink-0 ${isOpen ? 'w-64' : 'w-0'} transition-width duration-300 ease-in-out overflow-hidden`}>
+          <div className={`flex-shrink-0 ${isOpen ? 'w-64' : 'w-14'} transition-width duration-300 ease-in-out overflow-hidden`}>
             <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
           </div>
           <div className='flex flex-1 overflow-x-hidden overflow-y-auto'>
--- odyssey-ui/src/app/sidebar.js
+++ odyssey-ui/src/app/sidebar.js
@@ -1,21 +1,51 @@
 // components/Sidebar.js
 import Link from 'next/link';
-import { FaBars } from 'react-icons/fa';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaChevronCircleLeft } from "react-icons/fa";
+import { FaChevronCircleRight } from "react-icons/fa";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { MdCardTravel } from "react-icons/md";
+import { MdOutlineAccountTree } from "react-icons/md";
+import { MdOutlineBrokenImage } from "react-icons/md";
+import { MdOutlineBrightnessLow } from "react-icons/md";
 const Sidebar = ({isOpen, toggleSidebar}) => {
   return (
-    <div className={`fixed min-h-screen left-0 bg-text w-64 overflow-x-hidden transition-transform duration-300 ease-in-out transform ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-      <div className="flex items-center justify-between pl-4 py-4">
-        <span className="text-white text-xl font-bold">Sidebar</span>
-        {!isOpen ? <FaBars size={20} className="text-text-dark cursor-pointer mr-4" onClick={toggleSidebar} /> :
-          <FaArrowLeft size={20} className="text-text-dark cursor-pointer mr-4" onClick={toggleSidebar} />}
+    <div className={`fixed min-h-screen left-0 bg-text w-64 overflow-x-hidden transition-transform duration-300 ease-in-out transform ${isOpen ? 'translate-x-0' : '-translate-x-[200px]'}`}>
+      <div className="flex items-center justify-end pl-4 py-4">
+        {!isOpen ? <FaChevronCircleRight size={20} className="text-white cursor-pointer mr-5" onClick={toggleSidebar} /> :
+          <FaChevronCircleLeft size={20} className="text-white cursor-pointer mr-4" onClick={toggleSidebar} />}
       </div>
-      <div className="pl-4 pr-1">
-        <Link href="/">
-          <span className="text-white block py-2">Home</span>
+      <div className={`pl-2 pr-2 transition-opacity duration-500 ease-in-out ${
+          isOpen ? 'opacity-100' : 'opacity-0'
+        }`}>
+        <Link href="/" className='block my-5 py-4 rounded-md pl-2 bg-background bg-opacity-20'>
+          <div className="flex items-center">
+            <MdOutlineDashboardCustomize size={18} className="text-white cursor-pointer mr-5" />
+            <span className="text-white block text-[15px] font-semibold">Dashboard</span>
+          </div>
         </Link>
-        <Link href="/about">
-          <span className="text-white block py-2">About</span>
+        <Link href="/about" className='block my-3 py-4 pl-2'>
+          <div className="flex items-center">
+            <MdCardTravel size={18} className="text-white cursor-pointer mr-5" />
+            <span className="text-white block text-[15px] font-semibold">Projects</span>
+          </div>
+        </Link>
+        <Link href="/test" className='block my-3 py-4 pl-2'>
+          <div className="flex items-center">
+            <MdOutlineAccountTree size={18} className="text-white cursor-pointer mr-5" />
+            <span className="text-white block text-[15px] font-semibold">Test Creation</span>
+          </div>
+        </Link>
+        <Link href="/reports" className='block my-3 py-4 pl-2 '>
+          <div className="flex items-center">
+            <MdOutlineBrokenImage size={18} className="text-white cursor-pointer mr-5" />
+            <span className="text-white block text-[15px] font-semibold">Reports</span>
+          </div>
+        </Link>
+        <Link href="/reports" className='block my-3 py-4 pl-2'>
+          <div className="flex items-center">
+            <MdOutlineBrightnessLow size={18} className="text-white cursor-pointer mr-5" />
+            <span className="text-white block text-[15px] font-semibold">Settings</span>
+          </div>
         </Link>
         {/* Add more links as needed */}
       </div>
