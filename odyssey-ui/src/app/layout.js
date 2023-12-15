3986691bd75860aa20bbaa147ba8d5ceef70421e
Pasindu dineth <pasindudinethpeiris@gmail.com>
2023-12-14 19:13:01 -0600

    feat: page creation

--- odyssey-ui/src/app/layout.js
+++ odyssey-ui/src/app/layout.js
@@ -3,10 +3,10 @@ import './globals.css'
 import Navbar from "./navbar"
 import Sidebar from './sidebar'
 import { useState } from 'react';
-
+import { usePathname } from 'next/navigation'
 export default function RootLayout({ children }) {
   const [isOpen, setIsOpen] = useState(true);
-
+  const pathname = usePathname()
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         <Navbar toggleSidebar={toggleSidebar} isOpen={isOpen}/>
         <div className='flex flex-1 overflow-hidden'>
           <div className={`flex-shrink-0 ${isOpen ? 'w-64' : 'w-14'} transition-width duration-300 ease-in-out overflow-hidden`}>
-            <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+            <Sidebar isOpen={isOpen} pathname={pathname} toggleSidebar={toggleSidebar}/>
           </div>
           <div className='flex flex-1 overflow-x-hidden overflow-y-auto'>
             {children}
--- odyssey-ui/src/app/sidebar.js
+++ odyssey-ui/src/app/sidebar.js
@@ -7,43 +7,46 @@ import { MdCardTravel } from "react-icons/md";
 import { MdOutlineAccountTree } from "react-icons/md";
 import { MdOutlineBrokenImage } from "react-icons/md";
 import { MdOutlineBrightnessLow } from "react-icons/md";
-const Sidebar = ({isOpen, toggleSidebar}) => {
+const Sidebar = ({isOpen, toggleSidebar, pathname}) => {
+  const isActiveLink = (href) => {
+    return pathname === href;
+  };
   return (
     <div className={`fixed min-h-screen left-0 bg-text w-64 overflow-x-hidden transition-transform duration-300 ease-in-out transform ${isOpen ? 'translate-x-0' : '-translate-x-[200px]'}`}>
-      <div className="flex items-center justify-end pl-4 py-4">
+      <div className="flex items-center justify-end pl-3 py-4">
         {!isOpen ? <FaChevronCircleRight size={20} className="text-white cursor-pointer mr-5" onClick={toggleSidebar} /> :
           <FaChevronCircleLeft size={20} className="text-white cursor-pointer mr-4" onClick={toggleSidebar} />}
       </div>
       <div className={`pl-2 pr-2 transition-opacity duration-500 ease-in-out ${
           isOpen ? 'opacity-100' : 'opacity-0'
         }`}>
-        <Link href="/" className='block my-5 py-4 rounded-md pl-2 bg-background bg-opacity-20'>
+        <Link href="/dashboard" className={`block my-3 py-3 pl-2 ${isActiveLink('/dashboard') ? 'bg-background rounded-md bg-opacity-20' : ''}`}>
           <div className="flex items-center">
-            <MdOutlineDashboardCustomize size={18} className="text-white cursor-pointer mr-5" />
+            <MdOutlineDashboardCustomize size={18} className="text-white cursor-pointer mr-4" />
             <span className="text-white block text-[15px] font-semibold">Dashboard</span>
           </div>
         </Link>
-        <Link href="/about" className='block my-3 py-4 pl-2'>
+        <Link href="/projects" className={`block my-3 py-3 pl-2 ${isActiveLink('/projects') ? 'bg-background rounded-md bg-opacity-20' : ''}`}>
           <div className="flex items-center">
-            <MdCardTravel size={18} className="text-white cursor-pointer mr-5" />
+            <MdCardTravel size={18} className="text-white cursor-pointer mr-4" />
             <span className="text-white block text-[15px] font-semibold">Projects</span>
           </div>
         </Link>
-        <Link href="/test" className='block my-3 py-4 pl-2'>
+        <Link href="/tests" className={`block my-3 py-3 pl-2 ${isActiveLink('/tests') ? 'bg-background rounded-md bg-opacity-20' : ''}`}>
           <div className="flex items-center">
-            <MdOutlineAccountTree size={18} className="text-white cursor-pointer mr-5" />
+            <MdOutlineAccountTree size={18} className="text-white cursor-pointer mr-4" />
             <span className="text-white block text-[15px] font-semibold">Test Creation</span>
           </div>
         </Link>
-        <Link href="/reports" className='block my-3 py-4 pl-2 '>
+        <Link href="/reports" className={`block my-3 py-3 pl-2 ${isActiveLink('/reports') ? 'bg-background rounded-md bg-opacity-20' : ''}`}>
           <div className="flex items-center">
-            <MdOutlineBrokenImage size={18} className="text-white cursor-pointer mr-5" />
+            <MdOutlineBrokenImage size={18} className="text-white cursor-pointer mr-4" />
             <span className="text-white block text-[15px] font-semibold">Reports</span>
           </div>
         </Link>
-        <Link href="/reports" className='block my-3 py-4 pl-2'>
+        <Link href="/settings" className={`block my-3 py-3 pl-2 ${isActiveLink('/settings') ? 'bg-background rounded-md bg-opacity-20' : ''}`}>
           <div className="flex items-center">
-            <MdOutlineBrightnessLow size={18} className="text-white cursor-pointer mr-5" />
+            <MdOutlineBrightnessLow size={18} className="text-white cursor-pointer mr-4" />
             <span className="text-white block text-[15px] font-semibold">Settings</span>
           </div>
         </Link>
