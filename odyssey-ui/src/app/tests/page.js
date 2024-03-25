29ab0078ed72d54640caccbce2bcdcc1ee1552c0
Pasindu dineth <pasindudinethpeiris@gmail.com>
2024-03-24 18:24:49 -0600

    feat: nlp upgreade

--- odyssey-ui/src/app/tests/page.js
+++ odyssey-ui/src/app/tests/page.js
@@ -3,16 +3,35 @@ import { Badge } from 'flowbite-react';
 import TestCaseItem from "../../utils/components/test-case-item/testCaseItem"
 import React from 'react';
 import { useState } from 'react';
+import debounce from 'lodash/debounce';
+import axios from 'axios';
 export default function Tests() {
     const [tests, setTests] = useState(2);
+    const [isLoadingData, setIsLoadingData] = useState({testCaseID: 0, isLoading: false, suggessions: []});
 
     const addTestItem = () => {
       setTests(tests + 1);
     };
 
-    const onSearch = () => {
-      // get data from BE
-    }
+    const onSearch = debounce(async (searchTerm) => {
+      try {
+        setIsLoadingData({ testCaseID: searchTerm.testCaseId, isLoading: true, suggessions: [] });
+        const response = await axios.post('http://localhost:5000/process-nlp', {
+          input_text: searchTerm.term
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        
+        console.log('Response from server:', response.data);
+        setIsLoadingData({ testCaseID: searchTerm.testCaseId, isLoading: false, suggessions: response.data });
+        // Process the response data here
+      } catch (error) {
+        console.error('Error sending data to server:', error);
+        setIsLoadingData({ testCaseID: searchTerm.testCaseId, isLoading: false, suggessions: [] });
+      }
+    }, 300);
     return (
       <div className="flex w-full flex-col mx-5">
         <div className="flex justify-start w-full mt-5 p-5 space-x-20 bg-white">
@@ -39,7 +58,7 @@ export default function Tests() {
         <div className="flex w-full mt-2 min-h-screen bg-white flex-col">
           <div className='flex flex-col w-[65%]'>
             {[...Array(tests)].map((_, i, arr) => 
-              <TestCaseItem key={i + 1} count={i + 1} onSearch={onSearch} addTestItem={addTestItem} addEnabled={i === arr.length - 1}/>
+              <TestCaseItem testcaseID={i + 1} count={i + 1} onSearch={onSearch} addTestItem={addTestItem} addEnabled={i === arr.length - 1} isLoadingData={isLoadingData}/>
             )}
           </div>
           <div className="flex w-[35%] border-l-4">
--- server/models/db.js
+++ server/models/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Root@123',
-    database: 'odyssey',
+    password: '',
+    database: 'odyssydb',
     port: 3306
   });
     connection.on('connect', () => {
